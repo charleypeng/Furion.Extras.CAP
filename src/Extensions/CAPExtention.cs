604b1c60c60c0d8a76d2adadbbe256cb631e40f9
Peng Lei <charleypeng@hotmail.com>
2022-04-04 14:01:07 +0800

    Update CAPExtention.cs

--- src/Extensions/CAPExtention.cs
+++ src/Extensions/CAPExtention.cs
@@ -6,22 +6,22 @@ using System.Linq;
 namespace Microsoft.Extensions.DependencyInjection
 {
     /// <summary>
-    /// 对象映射拓展类
+    /// CAP Extention class
     /// </summary>
     public static class CAPExtension
     {
         /// <summary>
-        /// 添加对象映射
+        /// Add furion dotnet cap
         /// </summary>
-        /// <param name="services">服务集合</param>
-        /// <param name="option">CAP配置</param>
+        /// <param name="services">service collection</param>
+        /// <param name="option">CAP option</param>
         /// <returns></returns>
         public static IServiceCollection AddFurionCap(this IServiceCollection services,Action<CapOptions> option)
         {
             var assemblies = App.Assemblies.ToArray();
             var builder = services.AddCap(option);
             
-            // 扫描所有继承  ICapSubscriber 接口的对象映射配置
+            // scan all  ICapSubscriber
             if (assemblies != null && assemblies.Length > 0) builder.AddSubscriberAssembly(assemblies);
 
             return services;
